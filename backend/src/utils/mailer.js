d147ef5f376183fd91c2827939dbbb3a7ad485d0
DhwaniVyas <sounddiametre47@gmail.com>
2026-09-17 16:02:19 +0530

    Project refinement 2.0

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,23 +1,62 @@
+// const nodemailer = require("nodemailer");
+// const { env } = require("../config/env");
+
+// const transporter = nodemailer.createTransport({
+//   host: env.SMTP_HOST,
+//   port: env.SMTP_PORT,
+//   secure: env.SMTP_SECURE,
+//   auth: {
+//     user: env.SMTP_USER,
+//     pass: env.SMTP_PASS,
+//   },
+// });
+
+// async function sendEmail({ to, subject, html }) {
+//   await transporter.sendMail({
+//     from: env.MAIL_FROM,
+//     to,
+//     subject,
+//     html,
+//   });
+// }
+
+// module.exports = { sendEmail }; 
+
+
 const nodemailer = require("nodemailer");
 const { env } = require("../config/env");
 
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
-  port: env.SMTP_PORT,
-  secure: env.SMTP_SECURE,
+  port: Number(env.SMTP_PORT),
+  secure: env.SMTP_SECURE === "true",
   auth: {
     user: env.SMTP_USER,
     pass: env.SMTP_PASS,
   },
 });
 
+transporter.verify()
+  .then(() => {
+    console.log("[SMTP TEST] Connection successful");
+  })
+  .catch((error) => {
+    console.error("[SMTP TEST] Connection failed:", error.message);
+  });
+
 async function sendEmail({ to, subject, html }) {
-  await transporter.sendMail({
+  console.log(`[SMTP TEST] Sending email to ${to}`);
+
+  const info = await transporter.sendMail({
     from: env.MAIL_FROM,
     to,
     subject,
     html,
   });
+
+  console.log(`[SMTP TEST] Email sent: ${info.messageId}`);
+
+  return info;
 }
 
 module.exports = { sendEmail };
